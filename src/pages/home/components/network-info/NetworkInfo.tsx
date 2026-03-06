a64a16eeda75305805fc16fdf1d0fe3e95a2f819
JoaoBoscoDuarte <joaoboscoduartef@gmail.com>
2026-03-06 09:09:56 -0300

    feature: typesOfCersAndDeficiencies to two components

--- src/pages/home/components/network-info/NetworkInfo.tsx
+++ src/pages/home/components/network-info/NetworkInfo.tsx
@@ -1,10 +1,12 @@
 import WhatIsRCPD from "./section/WhatIsRCPD";
-import TypesOfCersAndDeficiencies from "./section/TypesOfCersAndDeficiencies";
+import TypesOfCersAndDeficiencies from "./section/TypesOfCers";
+import TypesOfDeficiencies from "./section/TypesOfDeficiencies";
 import ProfessionalsRoles from "./section/ProfessionalsRoles";
 import HistoryTimeline from "./section/HistoryTimeline";
 import AttentionLevel from "./section/AttentionLevel";
 import CersCards from "./section/CersCards";
 import WaveDivider from "../../../../components/WaveDivider";
+import TypesOfCers from "./section/TypesOfCers";
 
 interface NetworkInfoProps {
   showFlow: [boolean, number | null];
@@ -19,23 +21,42 @@ export default function NetworkInfo({
     <div id="network-info">
       <div className="bg-[var(--cor-bg-1)] text-white pt-16">
         <WhatIsRCPD />
-        <WaveDivider primaryColor="var(--cor-bg-1)" secondaryColor="var(--cor-bg-2)" />
+        <WaveDivider
+          primaryColor="var(--cor-bg-1)"
+          secondaryColor="var(--cor-bg-2)"
+        />
       </div>
-      <div className="bg-[var(--cor-bg-2)] text-white pt-16">
+      <div className=" text-white pt-16">
         <AttentionLevel />
-        <WaveDivider primaryColor="var(--cor-bg-2)" secondaryColor="var(--cor-bg-3)" />
+        <WaveDivider primaryColor="" secondaryColor="var(--cor-bg-3)" />
       </div>
       <div className="bg-[var(--cor-bg-3)] text-white pt-16">
-        <TypesOfCersAndDeficiencies />
-        <WaveDivider primaryColor="var(--cor-bg-3)" secondaryColor="var(--cor-bg-1)" />
+        <TypesOfCers />
+        <WaveDivider
+          primaryColor="var(--cor-bg-3)"
+          secondaryColor="var(--cor-bg-1)"
+        />
+      </div>
+      <div className="bg-[var(--cor-br-1)]">
+        <TypesOfDeficiencies />
+        <WaveDivider
+          primaryColor="var(--cor-bg-1)"
+          secondaryColor="var(--cor-bg-2)"
+        />
       </div>
       <div className="bg-[var(--cor-bg-1)] text-white pt-16">
         <CersCards showFlow={showFlow} setShowFlow={setShowFlow} />
-        <WaveDivider primaryColor="var(--cor-bg-1)" secondaryColor="var(--cor-bg-2)" />
+        <WaveDivider
+          primaryColor="var(--cor-bg-1)"
+          secondaryColor="var(--cor-bg-2)"
+        />
       </div>
       <div className="bg-[var(--cor-bg-2)] text-white pt-16">
         <HistoryTimeline />
-        <WaveDivider primaryColor="var(--cor-bg-2)" secondaryColor="var(--cor-bg-3)" />
+        <WaveDivider
+          primaryColor="var(--cor-bg-2)"
+          secondaryColor="var(--cor-bg-3)"
+        />
       </div>
       <div className="bg-[var(--cor-bg-3)] text-white pt-16">
         <ProfessionalsRoles />
